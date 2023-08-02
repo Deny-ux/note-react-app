9476206fc15345bf7a9dc1b26e73704a69886c75
Deny-ux <denys.fokashchuk@gmail.com>
2023-08-02 17:44:04 +0200

    Added summary table

--- src/components/SummaryRow.tsx
+++ src/components/SummaryRow.tsx
@@ -1,8 +1,19 @@
+import { icons } from "../icons";
 import { SummaryRowType } from "../generalTypes/types";
+
 type PropsType = {
   notePerCategory: SummaryRowType;
 };
 const SummaryRow = ({ notePerCategory }: PropsType) => {
-  return <div>SummaryRow</div>;
+  return (
+    <article className="single-summary-row">
+      <div className="task-name">
+        {icons[notePerCategory.category]}
+        <p>{notePerCategory.category}</p>
+      </div>
+      <p>{notePerCategory.active}</p>
+      <p>{notePerCategory.archived}</p>
+    </article>
+  );
 };
 export default SummaryRow;
--- src/icons.tsx
+++ src/icons.tsx
@@ -4,12 +4,8 @@ import {
   BsFillCartFill,
 } from "react-icons/bs";
 import { RiEditFill } from "react-icons/ri";
-import { GrEdit } from "react-icons/gr";
 import { BiSolidArchiveIn } from "react-icons/bi";
 import { FaHeadSideVirus } from "react-icons/fa";
-import { JsxElement } from "typescript";
-import { IconType } from "react-icons";
-import React from "react";
 
 export const icons = {
   delete: <BsFillTrashFill className="note-icon note-control-icon" />,
